8ee3d7ea6737027156eb8275e24ed82c072d5c8b
Kyo <kyo.cao@refinedchina.com>
2014-03-19 14:19:47 +0800

    test

---
+++ test/src/test/test.java
@@ -0,0 +1,14 @@
+package test;
+
+import java.util.regex.Pattern;
+
+public class test {
+
+	public static void main(String[] args) {
+	    String patternStop="停止|停下|停下来"; 
+        boolean result = Pattern.matches(patternStop, "停下来"); 
+		// TODO Auto-generated method stub
+		System.out.print(result);
+	}
+
+}
